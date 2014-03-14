a521ab2ed850b23b2b991885849507b7b81aedb3
phaer <phaer@phaer.org>
2014-03-14 18:33:17 +0100

    initial commit.

---
+++ lib/CreditCardController.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace Drupal\stripe_payment;
+
+class CreditCardController implements \Drupal\webform_paymethod_select\PaymentRecurrentController {
+  public $controller_data_defaults = array(
+    'private_key' => '',
+    'public_key'  => '',
+  );
+
+  public function __construct() {
+    $this->title = t('Stripe Credit Card');
+    $this->form = new \Drupal\payment_forms\CreditCardForm();
+
+    $this->payment_configuration_form_elements_callback = 'payment_forms_method_form';
+    $this->payment_method_configuration_form_elements_callback = '\Drupal\stripe_payment\configuration_form';
+
+  }
+
+  public function validate(\Payment $payment, \PaymentMethod $payment_method, $strict) {
+    // convert amount to cents.
+    foreach ($payment->line_items as $name => &$line_item) {
+      $line_item->amount = $line_item->amount * 100;
+    }
+  }
+
+  public function execute(\Payment $payment) {
+    $context = &$payment->context_data['context'];
+    $api_key = $payment->method->controller_data['private_key'];
+
+    switch ($context->value('donation_interval')) {
+      case 'm': $interval = '1 MONTH'; break;
+      case 'y': $interval = '1 YEAR'; break;
+      default:  $interval = NULL; break;
+    }
+  }
+
+  public function getName($context) {
+    return trim(
+      $context->value('title') . ' ' .
+      $context->value('first_name') . ' ' .
+      $context->value('last_name')
+    );
+  }
+
+  /**
+   * Helper for entity_load().
+   */
+  public static function load($entities) {
+    $pmids = array();
+    foreach ($entities as $method) {
+      if ($method->controller instanceof CommonController) {
+        $pmids[] = $method->pmid;
+      }
+    }
+    if ($pmids) {
+      $query = db_select('stripe_payment_payment_method_controller', 'controller')
+        ->fields('controller')
+        ->condition('pmid', $pmids);
+      $result = $query->execute();
+      while ($data = $result->fetchAssoc()) {
+        $method = $entities[$data['pmid']];
+        unset($data['pmid']);
+        $method->controller_data = (array) $data;
+        $method->controller_data += $method->controller->controller_data_defaults;
+      }
+    }
+  }
+
+  /**
+   * Helper for entity_insert().
+   */
+  public function insert($method) {
+    $method->controller_data += $this->controller_data_defaults;
+
+    $query = db_insert('stripe_payment_payment_method_controller');
+    $values = array_merge($method->controller_data, array('pmid' => $method->pmid));
+    $query->fields($values);
+    $query->execute();
+  }
+
+  /**
+   * Helper for entity_update().
+   */
+  public function update($method) {
+    $query = db_update('stripe_payment_payment_method_controller');
+    $values = array_merge($method->controller_data, array('pmid' => $method->pmid));
+    $query->fields($values);
+    $query->condition('pmid', $method->pmid);
+    $query->execute();
+  }
+
+  /**
+   * Helper for entity_delete().
+   */
+  public function delete($method) {
+    db_delete('stripe_payment_payment_method_controller')
+      ->condition('pmid', $method->pmid)
+      ->execute();
+  }
+}
+
+/* Implements PaymentMethodController::payment_method_configuration_form_elements_callback().
+ *
+ * @return array
+ *   A Drupal form.
+ */
+function configuration_form(array $form, array &$form_state) {
+  $controller_data = $form_state['payment_method']->controller_data;
+
+  $library = libraries_detect('stripe-php');
+  if (empty($library['installed'])) {
+    drupal_set_message($library['error message'], 'error', FALSE);
+  }
+
+  $form['private_key'] = array(
+    '#type' => 'textfield',
+    '#title' => t('Private key'),
+    '#description' => t('Available from Your Account / Settings / API keys on stripe.com'),
+    '#required' => true,
+    '#default_value' => isset($controller_data['private_key']) ? $controller_data['private_key'] : '',
+  );
+
+  $form['public_key'] = array(
+    '#type' => 'textfield',
+    '#title' => t('Public key'),
+    '#description' => t('Available from Your Account / Settings / API keys on stripe.com'),
+    '#required' => true,
+    '#default_value' => isset($controller_data['public_key']) ? $controller_data['public_key'] : '',
+  );
+
+  return $form;
+}
+
+/**
+ * Implements form validate callback for
+ * \stripe_payment\configuration_form().
+ */
+function configuration_form_validate(array $element, array &$form_state) {
+  $values = drupal_array_get_nested_value($form_state['values'], $element['#parents']);
+  $form_state['payment_method']->controller_data['private_key'] = $values['private_key'];
+  $form_state['payment_method']->controller_data['public_key'] = $values['public_key'];
+}
+
+}
