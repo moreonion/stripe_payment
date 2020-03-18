fa8dbffd1706cc7a7579e663dd85c54775ec81ec
Roman Zimmermann <roman@more-onion.com>
2020-03-18 16:37:53 +0100

    Use stored mandate info when rendering the token.

--- tests/TokenIntegrationTest.php
+++ tests/TokenIntegrationTest.php
@@ -16,18 +16,11 @@ class TokenIntegrationTest extends DrupalUnitTestCase {
     $controller = payment_method_controller_load('stripe_payment_sepa');
     $method = entity_create('payment_method', ['controller' => $controller]);
     $method->controller_data['creditor_id'] = 'TEST-CREDITOR-ID';
-    $api = $this->createMock(Api::class);
-    $api->method('retrieveIntent')->willReturn([
-      'mandate' => 'test_mandate_id',
-      'payment_method' => 'test_payment_method_id',
-    ]);
-    $mandate['payment_method_details']['sepa_debit']['reference'] = 'TEST-SEPA-REFERENCE';
-    $api->method('retrieveMandate')->willReturn($mandate);
-    $pm['sepa_debit']['last4'] = '1234';
-    $api->method('retrievePaymentMethod')->willReturn($pm);
-    $method->api = $api;
     $payment = entity_create('payment', ['method' => $method]);
-    $payment->stripe = ['stripe_id' => 'seti_1GMDoqISFtZjFMJ3oSNcAvSS'];
+    $payment->stripe_sepa = [
+      'mandate_reference' => 'TEST-SEPA-REFERENCE',
+      'last4' => '1234',
+    ];
     $result = token_replace('[payment:stripe-sepa-mandate-info]', ['payment' => $payment]);
     $expected = <<<HTML
 <h3>SEPA-Information</h3>
